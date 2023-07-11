32c8396839c0d48a8695e6ba1562265191ea6187
Jesse Sandhu <s.jesse23@gmail.com>
2023-07-10 21:33:16 -0700

    reformat code to avoid sql injection

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,62 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'jessesandhu',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+// pool.query(`
+// SELECT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistances
+// FROM teachers
+// JOIN assistance_requests ON teacher_id = teachers.id
+// JOIN students ON student_id = students.id
+// JOIN cohorts ON cohort_id = cohorts.id
+// WHERE cohorts.name = 'JUL02'
+// GROUP BY teachers.name, cohorts.name --why two group by?
+// ORDER BY teacher;
+// `)
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+//   })
+// }).catch(err => console.error('query error', err.stack));
+
+// vs
+
+// pool.query(`
+// SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+// FROM teachers
+// JOIN assistance_requests ON teacher_id = teachers.id
+// JOIN students ON student_id = students.id
+// JOIN cohorts ON cohort_id = cohorts.id
+// WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+// ORDER BY teacher;
+// `)
+// .then(res => {
+//   res.rows.forEach(row => {
+//     console.log(`${row.cohort}: ${row.teacher}`);
+//   })
+// });
+
+
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = $1
+ORDER BY teacher;
+  `;
+
+  const cohortName = process.argv[2];
+// Store all potentially malicious values in an array.
+const values = [`${cohortName}`];
+
+pool.query(queryString, values).then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+}).catch(err => console.error('query error', err.stack)); 
